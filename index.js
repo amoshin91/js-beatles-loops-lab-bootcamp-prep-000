b5f07988ea48955a065362f0adeb55d8513be8eb
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-11-12 23:01:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function johnLennonFacts() {
   let i = 0
   var lennonFacts = [];
   while (i < facts.length) {
-    lennonFacts = facts + '!!!'
+    lennonFacts[i] = facts + '!!!'
     i++
   }
   return lennonfacts;
